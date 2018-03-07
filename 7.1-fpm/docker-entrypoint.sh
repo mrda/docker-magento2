9b5fd4e26a930ac5955d776d83290c1fb0b36bcf
Michael Davies <michael@the-davies.net>
2018-03-07 15:55:47 +1030

    Adding in defaulted build-specific files

--- 7.1-fpm/docker-entrypoint.sh
+++ 7.1-fpm/docker-entrypoint.sh
@@ -38,10 +38,6 @@ if [ "$ENABLE_SENDMAIL" == "true" ]; then
     /etc/init.d/sendmail start
 fi
 
-# Substitute in php.ini values
-[ ! -z "${PHP_MEMORY_LIMIT}" ] && sed -i "s/!PHP_MEMORY_LIMIT!/${PHP_MEMORY_LIMIT}/" /usr/local/etc/php/conf.d/zz-magento.ini
-[ ! -z "${UPLOAD_MAX_FILESIZE}" ] && sed -i "s/!UPLOAD_MAX_FILESIZE!/${UPLOAD_MAX_FILESIZE}/" /usr/local/etc/php/conf.d/zz-magento.ini
-
 [ "$PHP_ENABLE_XDEBUG" = "true" ] && \
     docker-php-ext-enable xdebug && \
     echo "Xdebug is enabled"
